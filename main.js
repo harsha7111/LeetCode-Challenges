6816b1a4ed3b0c75135097dce3faca21beb7bf7e
Harsha <68165196+harsha7111@users.noreply.github.com>
2020-08-03 19:47:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-var reverseWords = function(s) {
-   let array = s.trim().split(' ').reverse();
-   return array.filter(value => value !== "").join(" ");
+var detectCapitalUse = function(word) {
+   let array = word.split('');
+    if(array.every(value => value === value.toLowerCase()) || array.every(value => value === value.toUpperCase()) ){
+        return true;
+    }
+    
+    if(array[0]=== array[0].toUpperCase()) {
+        array.splice(0,1);
+        if(array.every(value => value === value.toLowerCase()) )
+            return true;
+        else 
+            return false;
+    }
+    else {
+         return false;
+    }
+           
 };
